25dc293e9af42bd8020abb7b6a41868e0dbb5342
DX <2245724889@qq.com>
2023-05-02 15:43:46 +0800

    日志模块：添加事件机制。

--- tao/config.h
+++ tao/config.h
@@ -14,6 +14,7 @@
 #include<set>
 #include<unordered_map>
 #include<unordered_set>
+#include<functional>
 
 namespace tao{
 
@@ -32,6 +33,7 @@ public:
 
     virtual std::string toString() = 0;
     virtual bool fromString(const std::string& val) = 0;
+    virtual std::string getTypeName()const = 0;
 private:
     std::string m_name;
     std::string m_description;
@@ -261,7 +263,7 @@ template<class T, class FromStr = LexicalCast<std::string, T>
 class ConfigVar:public ConfigVarBase{
 public:
     typedef std::shared_ptr<ConfigVar> ptr;
-
+    typedef std::function<void (const T& old_value, const T& new_value)> on_change_cb;
     ConfigVar(const std::string& name, const T& default_value, const std::string& description)
         :ConfigVarBase(name, description)
         ,m_val(default_value){
@@ -300,9 +302,37 @@ public:
     }
 
     const T getValue() const {return m_val;}
-    void setValue(const T& v) {m_val = v;}
+    void setValue(const T& v) {
+        if(v == m_val){
+            return;
+        }
+        for(auto& i : m_cbs){
+            i.second(m_val, v);
+        }
+        m_val = v;
+    }
+    std::string getTypeName() const override { return typeid(T).name();}
+
+    void addListener(uint64_t key, on_change_cb cb){
+        m_cbs[key] = cb;
+    }
+
+    void delListener(uint64_t key){
+        m_cbs[key].erase(key);
+    }
+
+    on_change_cb getListener(uint64_t key){
+        auto it = m_cbs.find(key);
+        return it == m_cbs.end()? nullptr : it->second;
+    }
+
+    void clearListener(){
+        m_cbs.clear();
+    }
 private:
     T m_val;
+    //变更回调函数组，uint64_t key, 要求唯一，一般可以用hash
+    std::map<uint64_t, on_change_cb> m_cbs;
 };
 
 class Config{
@@ -319,7 +349,9 @@ public:
                     TAO_LOG_INFO(TAO_LOG_ROOT())<<"Lookup name="<<name<<" exists";
                     return tmp;
                 }else{
-                    TAO_LOG_ERROR(TAO_LOG_ROOT())<<"Lookup name="<<name<<" exists but type not "<<typeid(T).name();
+                    TAO_LOG_ERROR(TAO_LOG_ROOT())<<"Lookup name="<<name<<" exists but type not "<<typeid(T).name()
+                            <<"real_type="<<it->second->getTypeName()
+                            <<" "<<it->second->toString();
                     return nullptr;
                 }
             }
--- tests/test_config.cpp
+++ tests/test_config.cpp
@@ -99,11 +99,92 @@ void test_config(){
     XX_M(g_str_int_umap_value_config, str_int_umap, after);
 }
 
-int main(int argc, char** argv){
-    //TAO_LOG_INFO(TAO_LOG_ROOT()) << g_int_value_config->getValue();
-    //TAO_LOG_INFO(TAO_LOG_ROOT()) << g_int_value_config->toString();
+class Person {
+public:
+    Person(){}
+    std::string m_name;
+    int m_age = 0;
+    bool m_sex = 0;
 
+    std::string toString()const{
+        std::stringstream ss;
+        ss << "[Person name="<<m_name
+           << " age="<<m_age
+           << " sex="<<m_sex
+           << "]";
+        return ss.str();
+    }
+
+    bool operator==(const Person& oth) const {
+        return m_name == oth.m_name
+                && m_age == oth.m_age
+                && m_sex == oth.m_sex;
+    }
+};
+
+namespace tao{
+template<>
+class LexicalCast<std::string, Person>{
+public:
+    Person operator()(const std::string& v){
+        YAML::Node node = YAML::Load(v);
+        Person p;
+        p.m_name = node["name"].as<std::string>();
+        p.m_age = node["age"].as<int>();
+        p.m_sex = node["sex"].as<bool>();
+        return p;
+    }
+};
+
+template<>
+class LexicalCast<Person, std::string>{
+public:
+    std::string operator() (const Person& v){
+        YAML::Node node;
+        node["name"] = v.m_name;
+        node["age"] = v.m_age;
+        node["sex"] = v.m_sex;
+        std::stringstream ss;
+        ss<<node;
+        return ss.str();
+    }
+};
+}
+
+tao::ConfigVar<Person>::ptr g_person = 
+    tao::Config::Lookup("class.person", Person(), "system person");
+
+tao::ConfigVar<std::map<std::string, Person>>::ptr g_person_map = 
+    tao::Config::Lookup("class.map", std::map<std::string, Person>(), "system person");
+
+void test_class(){
+    TAO_LOG_INFO(TAO_LOG_ROOT())<<"before: "<<g_person->getValue().toString()<<" - "<<g_person->toString();
+
+#define XX_PM(g_var, prefix)\
+    {\
+        auto m = g_person_map->getValue(); \
+        for(auto& i:m){\
+            TAO_LOG_INFO(TAO_LOG_ROOT())<<prefix<<": "<<i.first<<" - "<<i.second.toString();\
+        }\
+        TAO_LOG_INFO(TAO_LOG_ROOT())<<prefix<<": size="<<m.size(); \
+    }
+
+    g_person->addListener(10, [](const Person& old_value, const Person& new_value){
+        TAO_LOG_INFO(TAO_LOG_ROOT())<<"old_value="<<old_value.toString()<<" new_value="<<new_value.toString();
+    });
+    XX_PM(g_person_map, "class.map before");
+    YAML::Node root = YAML::LoadFile("/home/nowcoder/Tao/bin/conf/log.yml");
+    tao::Config::LoadFromYaml(root);
+
+    TAO_LOG_INFO(TAO_LOG_ROOT())<<"after: "<<g_person->getValue().toString()<<" - "<<g_person->toString();
+    XX_PM(g_person_map, "class.map after");
+
+}
+
+int main(int argc, char** argv){
     //test_yaml();
-    test_config();
+    //test_config();
+
+    test_class();
     return 0;
 }
